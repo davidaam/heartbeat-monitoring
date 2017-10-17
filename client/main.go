5a481e8992e00014dc1981718d198a9b2625526c
David Atias <davidalejandro23@gmail.com>
2017-10-17 01:55:03 -0400

    Fixing database lock issue and reading client id from args

--- client/main.go
+++ client/main.go
@@ -6,9 +6,12 @@ import (
   "fmt"
   "log"
   "net"
+  "os"
 )
 
 func main() {
+  clientID := os.Args[1]
+
   sender := &heartbeat.HeartbeatSender { IP: "127.0.0.1", Port: 1234 }
   server := fmt.Sprintf("%s:%d", sender.IP, sender.Port)
   socket, err := net.Dial("udp", server)
@@ -20,7 +23,7 @@ func main() {
   defer sender.Socket.Close()
 
   for {
-    sender.Send(heartbeat.NewHeartbeat("Client A"))
+    sender.Send(heartbeat.NewHeartbeat(clientID))
     time.Sleep(time.Second)
   }
 }
--- server/main.go
+++ server/main.go
@@ -11,10 +11,10 @@ func main() {
   sqliteDB, _ := filepath.Abs("heartbeats.sqlite3")
 
   fmt.Printf("*** Heartbeats will be logged in %s ***\n\n", sqliteDB)
-  db := dbLogger.InitializeDB(sqliteDB)
+  db := dbLogger.InitializeDB(fmt.Sprintf("file:%s?cache=shared&mode=rwc", sqliteDB))
 
   receiveCallback := func (heartbeat *heartbeat.Heartbeat) {
-    fmt.Println("Heartbeat received")
+    fmt.Printf("Heartbeat received from %s\n", heartbeat.ClientID)
     go dbLogger.LogHeartbeat(db, heartbeat)
   }
   timeoutCallback := func (timeout int32) {
